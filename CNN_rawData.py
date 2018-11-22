b0034fb5f53ad226325680f60afefcbeb5bd3ea1
olramde <rladhkstn8@gmail.com>
2018-11-22 21:20:50 +0900

    CNN_rawData update

--- CNN_rawData.py
+++ CNN_rawData.py
@@ -11,14 +11,14 @@ import os
 
 # Hyperparameter
 parser = argparse.ArgumentParser()
-parser.add_argument('--nb_classes', dtype=int, default=4)
-parser.add_argument('--batch_size', dtype=int, default=128)
-parser.add_argument('--input_channels', dtype=int, default=1)
-parser.add_argument('--learning_rate', dtype=float, default=0.01)
-parser.add_argument('--num_epoch', dtype=int, default=5)
-parser.add_argument('--root_dataset', dtype=str, default='./dataset')
-parser.add_argument('--trainset_path', dtype=str, default='train.xlsx')
-parser.add_argument('--testset_path', dtype=str, default='test.xlsx')
+parser.add_argument('--nb_classes', type=int, default=4)
+parser.add_argument('--batch_size', type=int, default=128)
+parser.add_argument('--input_channels', type=int, default=1)
+parser.add_argument('--learning_rate', type=float, default=0.01)
+parser.add_argument('--num_epoch', type=int, default=5)
+parser.add_argument('--root_dataset', type=str, default='./dataset')
+parser.add_argument('--trainset_path', type=str, default='train.xlsx')
+parser.add_argument('--testset_path', type=str, default='test.xlsx')
 config = parser.parse_args()
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -32,8 +32,12 @@ class CustomDataset(Dataset):
         self.len = xy.shape[0]
         xyMatrix = xy.values # DataFrame to ndarray
 
-        self.x_data = torch.from_numpy(xy[:, 0:-1])
-        self.y_data = torch.from_numpy(xy[:, [-1]])
+        self.x_data = torch.from_numpy(xyMatrix[:, 0:-1])
+        self.y_data = torch.from_numpy(xyMatrix[:, [-1]])
+        print(self.x_data.shape, self.y_data.shape)
+        self.x_data = self.x_data.view(-1, 1, 128)
+        self.x_Data = self.y_data.view(-1, 1, 1)
+        print(self.x_data.shape, self.y_data.shape)
 
     def __getitem__(self, index):
         return self.x_data[index], self.y_data[index]
@@ -41,6 +45,7 @@ class CustomDataset(Dataset):
     def __len__(self):
         return self.len
 
+print("Data Preprocessing...")
 train_dataset = CustomDataset(train_path)
 test_dataset = CustomDataset(test_path)
 
@@ -50,6 +55,7 @@ train_loader = DataLoader(dataset=train_dataset,
 test_loader = DataLoader(dataset=test_dataset,
                          batch_size=config.batch_size,
                          shuffle=True)
+print("Complete")
         
 
 
@@ -58,24 +64,24 @@ class Model(nn.Module):
         super(Model, self).__init__()
         # Input Size : 1x128
         self.conv = nn.Sequential(
-            nn.Conv1d(input_channels, 8),
+            nn.Conv1d(input_channels, 8, kernel_size=3),
             nn.BatchNorm1d(8),
             nn.ReLU(),
-            nn.Conv1d(8, 16),
+            nn.Conv1d(8, 16, kernel_size=3),
             nn.BatchNorm1d(16),
             nn.ReLU(),
-            nn.Conv1d(16, 32),
+            nn.Conv1d(16, 32, kernel_size=3),
             nn.BatchNorm1d(32),
             nn.ReLU()
         )
         self.fc = nn.Sequential(
             nn.Dropout(0.5),
-            
+            nn.Linear(1000, nb_classes)
         )
     
     def forward(self, x):
         out = self.conv(x)
-        out.shape
+        print(out.shape)
         out = self.fc(out)
         
         return out
